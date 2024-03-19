5742a11a251fd1cb9e64eae05f4831d08f3449b9
bachirGDD <bachirguedouda@gmail.com>
2024-03-19 08:28:33 +0000

    finish implementation with div and mod with errors: bad_cast

--- tests/testfiles/65_div.c
+++ tests/testfiles/65_div.c
@@ -1,6 +1,7 @@
 int main() {
     int numerator = 20;
     int denominator = 4;
-    int result = numerator / denominator;
-    return result; // Expected to return 5
+    int result;
+    result = numerator / denominator;
+    return result; 
 }
