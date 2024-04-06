c4c6c3af7c3d73980c1d4f66d2667ff048e751f6
Gaspard3.6 <gaspard.serpinet@insa-lyon.fr>
2024-04-06 11:59:06 +0200

    add tests (. before the name to ignore)

---
+++ tests/testfiles/10_special_tests/188_ingnored_comments.c
@@ -0,0 +1,32 @@
+// this is a one line test \t\n
+
+int
+
+
+
+            main                
+            
+            
+            
+            
+                
+                    
+                    (
+
+
+// test
+
+/**/
+
+
+)                   {
+    /**
+    @define here is a multiple
+    lines
+test*/
+    return 0                ;
+
+//
+///
+}
+
