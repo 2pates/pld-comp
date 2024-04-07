595d017f5b1a02f18f15f9225ad6eef67eec1b37
“JamesSudlow” <jamessudlow4@gmail.com>
2024-04-07 21:56:36 +0200

    kind of works (they keep having core dumps)

---
+++ tests/testfiles/1_declaration/1004 undeclared use in return.c
@@ -0,0 +1,6 @@
+int main() {
+    int x;
+    x = 8;
+    x=x+1;
+    return y;
+}
---
+++ tests/testfiles/1_declaration/1006 only used in if.c
@@ -0,0 +1,9 @@
+int main() {
+    int x;
+    x = 8;
+    int y=0;
+    if(y>3){
+    	y=x;
+    }
+    return x;
+}
