cead66a0ad0836854304479d233be6fd064cf777
Gaspard3.6 <gaspard.serpinet@insa-lyon.fr>
2024-04-07 15:32:47 +0200

    add a test

--- tests/testfiles/9_control_structures/162_if_else_if.c
+++ tests/testfiles/9_control_structures/162_if_else_if.c
@@ -1,9 +1,14 @@
 int main() {
     int a;
     a = 42;
-    if (a == 42)
+    if (a > 30)
         a = 12;
-    else
+    else if (a > 40)
         a = 1;
+    else if (a < 20)
+        a = 4;
+    else
+        a = -5;
     return a;
 }
+
